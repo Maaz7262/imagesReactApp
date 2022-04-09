6e2f80e328522f51693c80d96481df7064455004
Maaz7262 <>
2022-04-09 11:28:58 -0700

    Prettier and eslint configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Search from "./components/Search";
 import CardImage from "./components/ImageCard";
 import Welcome from "./components/Welcome";
 import { useState } from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_API;
 
@@ -30,23 +30,26 @@ function App() {
   };
 
   const onClickHandle = (id) => {
-    setImages(images.filter((image) => (image.id !== id) ))
-  }
+    setImages(images.filter((image) => image.id !== id));
+  };
 
   return (
     <div className="App">
       <Header title="Images Gallery" />
       <Search onSubmit={onHandleSubmit} word={word} setWord={setWord} />
-      { images.length > 0 ? 
-      <Container className="mt-4">
-        <Row xs={1} md={2} lg={4}>
-          {images.map((image, i) => 
-          <Col key={i} className="p-2">
-              <CardImage  imgArr={image} clickHandler = {onClickHandle}/>
-          </Col>
-          )}
-        </Row>
-      </Container>: <Welcome />}
+      {images.length > 0 ? (
+        <Container className="mt-4">
+          <Row xs={1} md={2} lg={4}>
+            {images.map((image, i) => (
+              <Col key={i} className="p-2">
+                <CardImage imgArr={image} clickHandler={onClickHandle} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      ) : (
+        <Welcome />
+      )}
     </div>
   );
 }
